a749091fdeaab2a5e25d4f6102c918a1ee878848
Noah <noakanois@gmail.com>
2022-01-01 22:19:29 +0100

    graph

--- ratings.py
+++ ratings.py
@@ -1,9 +1,21 @@
 import berserk
 import numpy as np
 import matplotlib.pyplot as plt
-import datetime
+import datetime as dt
 import api_key
 
 token1 = api_key.token
 session = berserk.TokenSession(token1)
 client = berserk.Client(session=session)
+
+bullet_stats = client.users.get_rating_history("noakanoi")[0]["points"]
+
+bullet_rating = [x[3] for x in bullet_stats]
+bullet_date = [dt.date(x[0],x[1]+1,x[2]) for x in bullet_stats]
+
+plt.style.use('dark_background')
+plt.plot(bullet_date, bullet_rating)
+plt.ylabel("Rating")
+plt.xlabel("Date")
+plt.title("Bullet Rating")
+plt.show()
