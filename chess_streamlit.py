9758f8a7268ccc05209246d503c728e0a4b52ed5
Noah <noakanois@gmail.com>
2022-02-26 00:56:57 +0100

    adjust username

--- chess_streamlit.py
+++ chess_streamlit.py
@@ -7,34 +7,28 @@ import datetime as dt
 import api_key
 import plotly.express as px
 
-
 token1 = api_key.token
 session = berserk.TokenSession(token1)
 client = berserk.Client(session=session)
 
-st.title("Noakanoi Lichess Ratings")
+username = "lucjayjay"
 
+st.title(f"{username} Lichess Ratings")
 game_modes = ("Bullet", "Blitz", "Rapid", "Classical")
 selected_game_mode = st.selectbox("Choose a game mode", game_modes)
-
 game_mode_dict = {"Bullet":0, "Blitz":1, "Rapid":2, "Classical":3}
 
 @st.cache
-def load_data(mode):
-    
-    stats = client.users.get_rating_history("noakanoi")[game_mode_dict.get(mode)]["points"]
+def load_data(mode):   
+    stats = client.users.get_rating_history(username)[game_mode_dict.get(mode)]["points"]
     rating = [x[3] for x in stats]
     date = [dt.date(x[0],x[1]+1,x[2]) for x in stats]
     return pd.DataFrame(rating, date)
 
-
 data = load_data(selected_game_mode)
-
-
 def plot_data():
     fig = px.line(data, title=f"{selected_game_mode} Rating")
     fig.layout.update(title_text=f"{selected_game_mode} Rating", xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
-    
+    st.plotly_chart(fig)    
 plot_data()
     
